65229612680bf5710814059c4a008dbfb74e5afd
Linnea Saxvik <linnea.saxvik@hotmail.com>
2023-09-18 15:33:55 +0200

    forgot to wrap the app in authcontextprovider :,)

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import { BrowserRouter } from "react-router-dom"
+import AuthContextProvider from "./contexts/AuthContextProvider.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
