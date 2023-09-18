5b2742c0186ae807c607225922c6fa59c9d600f3
Mabi-xD <marcusbirgersson901123@gmail.com>
2023-09-18 14:56:51 +0200

    moved an import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import "./assets/scss/App.scss"
 import Navigation from "./components/Navaigation"
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
-import NotFound from "./pages/NotFound"import Restaurant_tips from './pages/RestaurantsIndexPage'
+import NotFound from "./pages/NotFound"
+import Restaurant_tips from './pages/RestaurantsIndexPage'
 
 const App = () => {
 	return (
