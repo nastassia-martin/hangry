e70eef9a20a99289cdb5760e4966f21e4e7ddd5b
Mabi-xD <marcusbirgersson901123@gmail.com>
2023-09-18 12:00:11 +0200

    added homepage, 404 page and some routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import './App.css'
+import './assets/scss/App.scss'
+import Navigation from './components/Navaigation'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
+
 
 const App = () => { 
   return (
       <div id="App">
+        <Navigation />
+
+		<Routes>
+			<Route path="*" element={<NotFound />} />
+			<Route path="/" element={<HomePage />} />
+			<Route path="/login" element={<NotFound />} />
+			<Route path="/sign_up" element={<NotFound />} />
+		</Routes>
+
      </div>
   )
 }
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+
+
+const NotFound = () => {
+
+    return (
+        <>
+            <h1>Cannot find the thing you are looking for. :( </h1>
+        </>
+    )
+}
+
+export default NotFound
