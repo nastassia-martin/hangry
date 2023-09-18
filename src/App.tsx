ff7dc7f1abf85354471774218d6c1fa54dbfc376
Mabi-xD <marcusbirgersson901123@gmail.com>
2023-09-18 13:16:47 +0200

    created a restaurant tips page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navigation from './components/Navaigation'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import NotFound from './pages/NotFound'
-
+import Restaurant_tips from './pages/RestaurantsIndexPage'
 
 const App = () => { 
   return (
@@ -15,6 +15,7 @@ const App = () => {
 			<Route path="/" element={<HomePage />} />
 			<Route path="/login" element={<NotFound />} />
 			<Route path="/sign_up" element={<NotFound />} />
+			<Route path="/tips" element={<Restaurant_tips />} />
 		</Routes>
 
      </div>
---
+++ src/pages/RestaurantsIndexPage.tsx
@@ -0,0 +1,12 @@
+
+
+
+const Restaurant_tips = () => {
+
+    return (
+        <h1>All them tips yaaow</h1>
+    )
+}
+
+export default Restaurant_tips
+
