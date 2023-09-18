36b6c4793bdc7967a8f9172db61300b4b924509f
nastassia martin <nastassia.martin@googlemail.com>
2023-09-18 14:31:09 +0200

    added generic getStreamCollection so that up to date data is shown and becuase we will have more that 1 collection

--- src/hooks/useGetEateries.ts
+++ src/hooks/useGetEateries.ts
@@ -1,39 +1,12 @@
-import { getDocs } from "firebase/firestore"
-import { Eateries } from "../types/restaurant.types"
-import { useEffect, useState } from "react"
+import { Eatery } from "../types/restaurant.types"
 import { restaurantsCol } from "../services/firebase"
+import useStreamCollection from "./useGetStreamColleciton"
 
 const useGetEateries = () => {
-	const [data, setData] = useState<Eateries | null>(null)
-	const [loading, setLoading] = useState(true)
-	const getData = async () => {
-		setLoading(true)
-
-		// get query snapshot of collection
-		const snapshot = await getDocs(restaurantsCol)
-
-		// loop over all restaurants
-		const data: Eateries = snapshot.docs.map((doc) => {
-			return {
-				...doc.data(),
-				_id: doc.id,
-			}
-		})
-
-		setData(data)
-		setLoading(false)
-	}
-
-	// Get data on component mount
-	useEffect(() => {
-		getData()
-	}, [])
-
-	return {
-		data,
-		useGetEateries,
-		loading,
-	}
+	return useStreamCollection<Eatery>(
+		restaurantsCol
+		// orderBy("distance"),
+	)
 }
 
 export default useGetEateries
