feaa4fa315f54fd4fa31729d028773ba81cfadd5
nastassia martin <nastassia.martin@googlemail.com>
2023-09-19 21:09:07 +0200

    cleaned up notes in map component

--- src/components/MainMap.tsx
+++ src/components/MainMap.tsx
@@ -1,22 +1,20 @@
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
 
 const MainMap = () => {
+	//load GoogleMapsAPI script
 	const { isLoaded } = useLoadScript({
-		// ref APIKey in env file
 		googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY,
 	})
+	// if it has not loaded show loading screen
 	if (!isLoaded) {
-		return <p>Loadinng... </p>
+		return <p>Loading... </p>
 	}
-	// require 3 props in GoogleMap
-	//1.zoom factor
-	//2. where should the map be centered (currently over Malmö)
-	//3. how big the contianer will be rendered into
+
 	return (
 		<GoogleMap
-			zoom={12}
-			center={{ lat: 55.5918001, lng: 13.0167039 }}
-			mapContainerClassName='main-map'
+			zoom={12} // set zoom over map
+			center={{ lat: 55.5918001, lng: 13.0167039 }} // where map should be centered
+			mapContainerClassName='main-map' // container size of where map will be rendered
 		>
 			{/*this marker should be the user position */}
 			<Marker position={{ lat: 55.5918001, lng: 13.0167039 }} />
