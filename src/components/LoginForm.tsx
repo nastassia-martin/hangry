3b40e3adc8438745a5557c5c632b10715d884c62
nastassia martin <nastassia.martin@googlemail.com>
2023-09-25 12:35:48 +0200

    veery basic autocomplete functionaliity

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,56 +1,66 @@
-import React, { useRef } from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { LoginCredentials } from '../types/user.types'
+import React, { useRef } from "react"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { LoginCredentials } from "../types/user.types"
 
 interface IFormProps {
-    onLogin: SubmitHandler<LoginCredentials>
-    loading: boolean
+	onLogin: SubmitHandler<LoginCredentials>
+	loading: boolean
 }
 
 const LoginForm: React.FC<IFormProps> = ({ onLogin: onSignup, loading }) => {
-    const { handleSubmit, register, watch, formState: { errors } } = useForm<LoginCredentials>()
-    const passwordRef = useRef('')
-    passwordRef.current = watch('password')
+	const {
+		handleSubmit,
+		register,
+		watch,
+		formState: { errors },
+	} = useForm<LoginCredentials>()
+	const passwordRef = useRef("")
+	passwordRef.current = watch("password")
 
-    return (
-        <Form onSubmit={handleSubmit(onSignup)}>
-            <Form.Group controlId='email' className='mb-3'>
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    placeholder='Your email here'
-                    type='email'
-                    {...register('email', {
-                        required: 'You have to enter a registered email',
-                    })}
-                />
-                {errors.email && <p>{errors.email.message ?? 'This is an invalid value'}</p>}
-            </Form.Group>
+	return (
+		<Form onSubmit={handleSubmit(onSignup)}>
+			<Form.Group controlId="email" className="mb-3">
+				<Form.Label>Email</Form.Label>
+				<Form.Control
+					placeholder="Your email here"
+					type="email"
+					{...register("email", {
+						required: "You have to enter a registered email",
+					})}
+				/>
+				{errors.email && (
+					<p>{errors.email.message ?? "This is an invalid value"}</p>
+				)}
+			</Form.Group>
 
-            <Form.Group controlId='password' className='mb-3'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type='password'
-                    autoComplete='new-password'
-                    {...register('password', {
-                        required: 'You need to enter your password',
-                        minLength: {
-                            value: 3,
-                            message: 'Your password is at least 6 characters',
-                        },
-                    })}
-                />
-                {errors.password && <p className='invalid'>{errors.password.message ?? 'Invalid value'}</p>}
-                <Form.Text>Enter your password</Form.Text>
-            </Form.Group>
+			<Form.Group controlId="password" className="mb-3">
+				<Form.Label>Password</Form.Label>
+				<Form.Control
+					type="password"
+					autoComplete="new-password"
+					{...register("password", {
+						required: "You need to enter your password",
+						minLength: {
+							value: 3,
+							message: "Your password is at least 6 characters",
+						},
+					})}
+				/>
+				{errors.password && (
+					<p className="invalid">
+						{errors.password.message ?? "Invalid value"}
+					</p>
+				)}
+				<Form.Text>Enter your password</Form.Text>
+			</Form.Group>
 
-
-            <Button variant='secondary' type='submit' disabled={loading}>
-                {loading ? 'Logging In' : 'Login'}
-            </Button>
-        </Form>
-    )
+			<Button variant="secondary" type="submit" disabled={loading}>
+				{loading ? "Logging In" : "Login"}
+			</Button>
+		</Form>
+	)
 }
 
 export default LoginForm
--- src/components/MainMap.tsx
+++ src/components/MainMap.tsx
@@ -3,9 +3,11 @@ import {
 	useJsApiLoader,
 	MarkerF,
 	InfoWindowF,
+	Autocomplete,
 } from "@react-google-maps/api"
+
 import useGetEateries from "../hooks/useGetEateries"
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
 import { Eatery } from "../types/restaurant.types"
 import RestaurantCard from "./RestaurantCard"
 import { faPerson } from "@fortawesome/free-solid-svg-icons"
@@ -24,10 +26,18 @@ const MainMap = () => {
 
 		// do stuff with map
 	}, [])
+	const options = useMemo(
+		() => ({
+			disableDefaultUI: true, // removes fullscreen
+			clickableIcons: false, // cannot click on anything other the markers in db
+		}),
+		[]
+	)
 
 	//load GoogleMapsAPI script
 	const { isLoaded, loadError } = useJsApiLoader({
 		googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY,
+		libraries: ["places"],
 	})
 	useEffect(() => {
 		// run this once to get the users position
@@ -61,64 +71,72 @@ const MainMap = () => {
 	}
 
 	return (
-		<GoogleMap
-			onLoad={onMapLoad}
-			zoom={12} // set zoom over map
-			center={{ lat: 55.5918001, lng: 13.0167039 }} // where map should be centered
-			mapContainerClassName="main-map" // container size of where map will be rendered
-		>
-			{/*this marker should be the user position */}
-			{userPosition && (
-				<MarkerF
-					position={userPosition}
-					icon={{
-						path: faPerson.icon[4] as string, // path to icon
-						anchor: new google.maps.Point(
-							faPerson.icon[0] / 2, // width
-							faPerson.icon[1] // height
-						),
-						scale: 0.075,
-						fillColor: "#004d65",
-						fillOpacity: 1,
-					}}
-					// make marker accessible
-					options={{
-						optimized: false,
-					}}
-					title="you are here"
-				/>
-			)}
-
-			{/**render restaurant markers */}
-			{data &&
-				data.map((restaurant) => (
+		<>
+			<Autocomplete>
+				<div>
+					<input type="text" />
+				</div>
+			</Autocomplete>
+			<GoogleMap
+				onLoad={onMapLoad}
+				zoom={12} // set zoom over map
+				center={{ lat: 55.5918001, lng: 13.0167039 }} // where map should be centered
+				mapContainerClassName="main-map" // container size of where map will be rendered
+				options={options}
+			>
+				{/*this marker should be the user position */}
+				{userPosition && (
 					<MarkerF
-						key={restaurant._id}
-						position={restaurant.location}
-						onClick={() => handleMarkerClick(restaurant)}
+						position={userPosition}
+						icon={{
+							path: faPerson.icon[4] as string, // path to icon
+							anchor: new google.maps.Point(
+								faPerson.icon[0] / 2, // width
+								faPerson.icon[1] // height
+							),
+							scale: 0.075,
+							fillColor: "#004d65",
+							fillOpacity: 1,
+						}}
 						// make marker accessible
 						options={{
 							optimized: false,
-							title: `${restaurant.address.restaurantName}`,
 						}}
-					>
-						{restaurant._id === selectedMarker?._id ? (
-							<InfoWindowF
-								onCloseClick={() => {
-									setSelectedMarker(null)
-								}}
-								options={{
-									ariaLabel: `${restaurant.address.restaurantName}`,
-									minWidth: 150,
-								}}
-								position={restaurant.location}
-							>
-								<RestaurantCard data={restaurant} />
-							</InfoWindowF>
-						) : null}
-					</MarkerF>
-				))}
-		</GoogleMap>
+						title="you are here"
+					/>
+				)}
+
+				{/**render restaurant markers */}
+				{data &&
+					data.map((restaurant) => (
+						<MarkerF
+							key={restaurant._id}
+							position={restaurant.location}
+							onClick={() => handleMarkerClick(restaurant)}
+							// make marker accessible
+							options={{
+								optimized: false,
+								title: `${restaurant.address.restaurantName}`,
+							}}
+						>
+							{restaurant._id === selectedMarker?._id ? (
+								<InfoWindowF
+									onCloseClick={() => {
+										setSelectedMarker(null)
+									}}
+									options={{
+										ariaLabel: `${restaurant.address.restaurantName}`,
+										minWidth: 150,
+									}}
+									position={restaurant.location}
+								>
+									<RestaurantCard data={restaurant} />
+								</InfoWindowF>
+							) : null}
+						</MarkerF>
+					))}
+			</GoogleMap>
+		</>
 	)
 }
 
