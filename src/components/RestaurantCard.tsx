1f462da89ba8b710e56325921bf1d60487c2a7e0
nastassia martin <nastassia.martin@googlemail.com>
2023-09-28 14:33:48 +0200

    added offering to restaurant card

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -12,6 +12,13 @@ const RestaurantCard: React.FC<IProps> = ({ data }) => {
 			<Card.Title>{data.address.restaurantName}</Card.Title>
 			<Card.Body className="restaurant-card-body">
 				<Card.Text>{data.description}</Card.Text>
+				<Card.Text>
+					{data.offering.afterWork && <span>After work</span>}
+					{data.offering.dinner && <span>Dinner</span>}
+					{data.offering.lunch && <span>Lunch</span>}
+					{data.offering.vegan && <span>Vegan</span>}
+					{data.offering.vegetarian && <span>Vegetarian</span>}
+				</Card.Text>
 				<Card.Link
 					target="_blank"
 					href={`https://www.google.com/maps?q=${data.address.addressNumber}+${data.address.street}+${data.address.city}`}
