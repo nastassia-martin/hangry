e21bd62ad874c470834912038fba75055f449194
nastassia martin <nastassia.martin@googlemail.com>
2023-09-28 14:21:35 +0200

    when user presses for directions in card, a new window is opened

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -12,7 +12,12 @@ const RestaurantCard: React.FC<IProps> = ({ data }) => {
 			<Card.Title>{data.address.restaurantName}</Card.Title>
 			<Card.Body className="restaurant-card-body">
 				<Card.Text>{data.description}</Card.Text>
-				<Card.Link href={`https://www.google.com/maps?q=${data.address.addressNumber}+${data.address.street}+${data.address.city}`}>Directions</Card.Link>
+				<Card.Link
+					target="_blank"
+					href={`https://www.google.com/maps?q=${data.address.addressNumber}+${data.address.street}+${data.address.city}`}
+				>
+					Directions
+				</Card.Link>
 			</Card.Body>
 		</Card>
 	)
